fec3fe90c4291e6fdd7e811c236af666e32f37e7
Daniel Leonardo <daniel.fnz@hotmail.com>
2019-01-27 16:33:41 -0200

    Configurando o intervalo do carousel

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: 12000, noPause: true, showIndicators: true } }
+  ]
 })
 export class SliderComponent implements OnInit {
 
