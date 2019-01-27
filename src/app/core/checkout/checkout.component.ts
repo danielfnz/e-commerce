4840faa52eb4f117a139a25eca3185c51c59f507
Daniel Leonardo <daniel.fnz@hotmail.com>
2019-01-27 19:17:57 -0200

    Começando o checkout

--- src/app/core/checkout/checkout.component.ts
+++ src/app/core/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as cepPromise from 'cep-promise/dist/cep-promise-browser';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  public cep = '';
+  public endereco = '';
+  public cidade = '';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+  }
+
+  buscarCep() {
+    this.http.get(`https://viacep.com.br/ws/${this.cep}/json/`).subscribe(retorno => {
+      console.log(retorno);
+      this.cidade = retorno['localidade'] + ' - ' + retorno['uf'];
+      this.endereco = retorno['logradouro'];
+    });
+  }
+
+
+  onKeydUp(event) {
+    if (event.key === 'Enter') {
+     this.buscarCep();
+    }
+
+    if (this.cep.length === 8) {
+      this.buscarCep();
+    }
+  }
+
+  onStep1Next(event) {
+
+  }
+
+  onStep2Next(event) {
+
+  }
+
+  onComplete(event) {
+
   }
 
 }
